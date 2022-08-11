d31d3db5d8793184b3b0d92a485dcd9d986f418a
Denis <pydenzk@gmail.com>
2022-08-11 22:16:41 +0300

    Lesson 4

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,6 @@
 from django.shortcuts import render
 from blog.models import Post
 # from django.http import HttpResponse
-#
-# # Create your views here.
-# def home(request):  # (1) функция-обработчик эндпоинта (вьюшка)
-#     return HttpResponse('<h1>Hello world</h1>')
-#
-# def about(request):
-#     return HttpResponse('<h1>Blog about</h1>')
-
-# возвращаем теперь не HttpResponse, а render, чтобы воспользоваться html-шаблоном
-
-# posts = [
-#     {
-#         'author': 'Max',
-#         'title': 'Blog Post 1',
-#         'content': 'First post content',
-#         'date_posted': 'Feb 25, 2022',
-#     },
-#     {
-#         'author': 'Ian',
-#         'title': 'Blog Post 2',
-#         'content': 'Second post content',
-#         'date_posted': 'Feb 23, 2022',
-#     },
-# ]
 
 def home(request):
     context = {
